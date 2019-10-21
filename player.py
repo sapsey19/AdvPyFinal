e08547317fa6f5cb683291be0c149124e4614502
Sean Apsey <ssapsey@mavs.coloradomesa.edu>
2019-10-21 09:55:16 -0600

    cleaned up files

--- player.py
+++ player.py
@@ -1,7 +1,7 @@
 import pygame
 import pygame.image as img
 
-#animations
+#list of animations
 walkUp = [img.load('res/runu1.png'), img.load('res/runu2.png'), img.load('res/runu3.png'), img.load('res/runu1.png'), img.load('res/runu2.png'), img.load('res/runu3.png')]
 walkDown = [img.load('res/rund1.png'), img.load('res/rund2.png'), img.load('res/rund3.png'), img.load('res/rund1.png'), img.load('res/rund2.png'), img.load('res/rund3.png')]
 walkLeft = [img.load('res/runl1.png'), img.load('res/runl2.png'), img.load('res/runl3.png'), img.load('res/runl1.png'), img.load('res/runl2.png'), img.load('res/runl3.png')]
@@ -10,6 +10,7 @@ walkRight = [img.load('res/runr1.png'), img.load('res/runr2.png'), img.load('res
 scaled_size = 1.5
 
 player_width, player_height = walkUp[0].get_size()
+#scales the player's size
 for i in range(0, len(walkUp)):
     temp = pygame.transform.scale(walkUp[i], (int(player_width*scaled_size), int(player_height*scaled_size)))
     walkUp[i] = temp
@@ -49,7 +50,7 @@ class player(object):
 
     def set_y(self, y):
         self.y = y
-
+    
     def get_top_collider(self):
         self.top_rect = pygame.Rect(self.x + 17, self.y + 20, 15, 30)
         return self.top_rect
@@ -63,22 +64,22 @@ class player(object):
         return self.right_rect
 
     def draw(self, window):
-        if self.walkCount >= 30:
+        if self.walkCount >= 60:
             self.walkCount = 0
         if self.left:
-            window.blit(walkLeft[self.walkCount//5], (self.x, self.y))
+            window.blit(walkLeft[self.walkCount//10], (self.x, self.y))
             self.walkCount += 1
             self.last_pressed = 0
         elif self.right:
-            window.blit(walkRight[self.walkCount//5], (self.x, self.y))
+            window.blit(walkRight[self.walkCount//10], (self.x, self.y))
             self.walkCount += 1
             self.last_pressed = 1
         elif self.up:
-            window.blit(walkUp[self.walkCount//5], (self.x, self.y))
+            window.blit(walkUp[self.walkCount//10], (self.x, self.y))
             self.walkCount += 1
             self.last_pressed = 2
         elif self.down:
-            window.blit(walkDown[self.walkCount//5], (self.x, self.y))
+            window.blit(walkDown[self.walkCount//10], (self.x, self.y))
             self.walkCount += 1
             self.last_pressed = 3
         else:
@@ -98,17 +99,14 @@ class player(object):
     def update(self):
         if self.left:
             self.xvel = -1
-
         if self.right:
             self.xvel = 1
         if self.up:
             self.yvel = -1
-
         if self.down:
             self.yvel = 1
 
-
-
+        #updates player's position
         self.x += self.xvel * self.speed
         self.y += self.yvel * self.speed
         self.xvel = 0
